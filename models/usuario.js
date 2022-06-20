aee8007cc5acde827f0980e939ec5c8e925d773f
HouzzyP <choclo99@gmail.com>
2022-06-20 14:22:57 -0300

    Google Sign added

--- models/usuario.js
+++ models/usuario.js
@@ -21,8 +21,9 @@ const UsuarioSchema = Schema({
     },
     rol: {
         type: String,
-        required: true,
-        emun: ['ADMIN_ROLE', 'USER_ROLE']
+        required: false,
+        emun: ['ADMIN_ROLE', 'USER_ROLE'],
+        default: 'USER_ROLE',
     },
     estado: {
         type: Boolean,
