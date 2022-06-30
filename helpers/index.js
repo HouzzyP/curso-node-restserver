93d19dfdbbe6088f0e9ba63d2f70000046928657
HouzzyP <choclo99@gmail.com>
2022-06-30 15:04:32 -0300

    fix tipyng problem

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,7 @@
 
 const dbValidators = require('./db-validators')
 const generarJWT = require('./generarJWT')
-const googleVerify = require('./google-Verify')
+const googleVerify = require('./google-verify')
 const subirArchivos = require('./subir-archivo')
 
 module.exports = {
